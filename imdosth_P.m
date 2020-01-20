3fb4b7b88a9524d691c92a0c2b0e17ecfc0afea6
IAASL <60064825+IAASL@users.noreply.github.com>
2020-01-20 13:35:22 +0800

    Delete imdosth_P.m
    duplicated upload

--- imdosth_P.m
+++
@@ -1,6 +0,0 @@
-% Function to acquire an image according to the filename string and process it with the imtrans_P
-% function to acquire the stretched version of that image.
-function [strtimg] = imdosth_P(filename)
-    im_temp = imread(filename);
-    strtimg = imtrans_P(im_temp);
-end
